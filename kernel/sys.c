e6d1987c5ba81fd4420725ef0aca7a346c992ed0
NEESCHAL-3 <Nischalkhadka022@gmail.com>
2026-07-25 23:07:49 +0545

    evonix: add PrismaAuthD kernel challenge provider
    Change-Id: I57819e0a5250d7e55be0066b1a4e1ca290a11302

--- kernel/sys.c
+++ kernel/sys.c
@@ -5,6 +5,7 @@
  *  Copyright (C) 1991, 1992  Linus Torvalds
  */
 
+#include <linux/evx_prisma.h>
 #include <linux/export.h>
 #include <linux/mm.h>
 #include <linux/mm_inline.h>
@@ -2477,6 +2478,9 @@ static int prctl_get_auxv(void __user *addr, unsigned long len)
 SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 		unsigned long, arg4, unsigned long, arg5)
 {
+    if (option == PR_EVX_PRISMA_CHALLENGE)
+        return evx_prisma_prctl(arg2, arg3, arg4, arg5);
+
 	struct task_struct *me = current;
 	unsigned char comm[sizeof(me->comm)];
 	long error;
